3f07222bfd395904843bec33411e1f5acd155898
MahendraVarma1 <mahendravarmakokkili@gmail.com>
2024-07-29 19:12:15 +0530

    Time: 36 ms (54.86%), Space: 16.4 MB (55.69%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        x = floor(num ** 0.5)
+
+        return x ** 2 == num
+        
+                
+             
+        
+
+
+        
